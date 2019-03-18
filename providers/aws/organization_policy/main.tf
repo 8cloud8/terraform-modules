9dbfe74babac331c305ba2d20796c104220e64e7
Admir Trakic <xomodo@gmail.com>
2019-03-18 14:11:23 +0100

    Add organization_policy

---
+++ providers/aws/organization_policy/main.tf
@@ -0,0 +1,34 @@
+# NOTE: Organization policy can only be created from the master account
+
+terraform {
+  required_version = ">= 0.11.2"
+}
+
+provider "aws" {
+  version = ">= 2.1.0"
+}
+
+data "aws_caller_identity" "current" {}
+
+variable "organisations_policy_name" {
+  description = "The name of organisation policy"
+}
+
+variable "organisations_policy_description" {
+  default = "Default organisation policy description"
+}
+
+variable "organizations_policy_content" {
+  description = "Default organisation policy content in JSON format"
+  default     = "{\"Version\": \"2012-10-17\", \"Statement\": { \"Effect\": \"Allow\", \"Action\": \"*\", \"Resource\": \"*\"}}"
+}
+
+resource "aws_organizations_policy" "policy" {
+  content     = "${var.organizations_policy_content}"
+  description = "${var.organisations_policy_description}"
+  name        = "${var.organisations_policy_name}"
+}
+
+output "arn" {
+  value = "${aws_organizations_policy.policy.arn}"
+}
