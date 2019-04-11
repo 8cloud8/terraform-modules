27b9114aa9a24176d313f9f3c785a004a4d277da
Admir Trakic <xomodo@gmail.com>
2019-04-11 21:49:02 +0200

    Fmt

--- providers/common/variables/main.tf
+++ providers/common/variables/main.tf
@@ -1,17 +1,18 @@
 terraform {
   required_version = ">= 0.11.2"
+
   #backend "local" {}
 }
 
 locals {
   environment = "${lookup(var.workspace_to_environment_map, terraform.workspace, "dev")}"
-  size = "${local.environment == "dev" ? lookup(var.workspace_to_size_map, terraform.workspace, "small") : var.environment_to_size_map[local.environment]}"
+  size        = "${local.environment == "dev" ? lookup(var.workspace_to_size_map, terraform.workspace, "small") : var.environment_to_size_map[local.environment]}"
 }
 
 output "env" {
- value = "${local.environment}"
+  value = "${local.environment}"
 }
 
 output "size" {
- value = "${local.size}"
+  value = "${local.size}"
 }
