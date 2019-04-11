7b5d5ea0f77c087e2a942ef91bd2c4a8a1ffdc9c
Admir Trakic <atr@ufst.dk>
2019-04-11 13:53:58 +0200

    Update

--- providers/common/env-defaults/vars.tf
+++ providers/common/env-defaults/vars.tf
@@ -1,3 +1,9 @@
+variable "enabled" {
+  type        = "string"
+  default     = "true"
+  description = "Set to false to prevent the module from creating any resources"
+}
+
 variable "workspace_to_environment_map" {
   type = "map"
 
